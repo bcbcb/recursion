98050304024a74b728fec258cef2083614cd117c
bcbcb <bcbcb@users.noreply.github.com>
2014-10-29 14:28:45 -0700

    Up to functions in objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,18 +5,51 @@
 var stringifyJSON = function(obj) {
     
   var result = '';
+  console.log("OBJECT -- " + typeof obj);
+  console.log(obj);
 
-  console.log("OBJECT*** : " + obj + ' - type: ' + typeof obj);
-
+  // Strings in double quotes
   if (typeof obj === 'string') {
     return '\"'  + obj + '\"';
   }
-  if (obj === null || typeof obj === 'number' || typeof obj === 'boolean') {
+
+  // Null, Number, Boolean
+  if (obj === null || typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'undefined' ) {
     return  obj + '';
   }
+
+
+  // Arrays
   if (Array.isArray(obj)) {
-    return '[' + obj.toString() + ']';
-}
-  
-  return result;
+    if (obj.length === 0) {
+      return '[]';
+    }
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]) + ','
+    } 
+    return '[' + result.slice(0, -1) + ']';
+  }
+
+  if (typeof obj === 'function') {
+    console.log('func');
+    return '{}';
+  }
+
+  // Objects
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    console.log('KEYS:');
+    console.log(Object.keys(obj));
+
+    for (var key in obj) {
+      if (obj[key] !== undefined) {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    return '{' + result.slice(0, -1) + '}';
+  }
+
+
 };
