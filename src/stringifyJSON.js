7fd425d6192765c5d7d6ba71032a61ff38849ae6
bcbcb <bcbcb@users.noreply.github.com>
2014-10-29 13:50:16 -0700

    Tests pass up to array with a number

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,20 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+    
+  var result = '';
+
+  console.log("OBJECT*** : " + obj + ' - type: ' + typeof obj);
+
+  if (typeof obj === 'string') {
+    return '\"'  + obj + '\"';
+  }
+  if (obj === null || typeof obj === 'number' || typeof obj === 'boolean') {
+    return  obj + '';
+  }
+  if (Array.isArray(obj)) {
+    return '[' + obj.toString() + ']';
+}
+  
+  return result;
 };
